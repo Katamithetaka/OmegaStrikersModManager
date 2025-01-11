6f8a28a57f2cf73e3fc5830c86ae79e53211c943
Takathedinosaur <katamithetaka@gmail.com>
2025-01-11 14:33:38 +0100

    I fucking hate you Visual Studio

--- OmegaStrikersModLoader/buildInstaller.bat
+++ OmegaStrikersModLoader/buildInstaller.bat
@@ -1,12 +1,12 @@
 @REM # download https://github.com/trumank/repak/releases/download/v0.2.2/repak_cli-x86_64-pc-windows-msvc.zip
 
-mkdir .\bin\Release\net8.0\repak\temp\windows
+mkdir .\bin\Release\net8.0-windows\repak\temp\windows
 
-call curl.exe -o ./bin/Release/net8.0/repak/temp/windows/repak_cli-x86_64-pc-windows-msvc.zip -fL "https://github.com/trumank/repak/releases/latest/download/repak_cli-x86_64-pc-windows-msvc.zip"
-call curl.exe -o ./bin/Release/net8.0/repak/temp/repak_cli-x86_64-unknown-linux-gnu.tar.xz -fL "https://github.com/trumank/repak/releases/latest/download/repak_cli-x86_64-unknown-linux-gnu.tar.xz"
+call curl.exe -o ./bin/Release/net8.0-windows/repak/temp/windows/repak_cli-x86_64-pc-windows-msvc.zip -fL "https://github.com/trumank/repak/releases/latest/download/repak_cli-x86_64-pc-windows-msvc.zip"
+call curl.exe -o ./bin/Release/net8.0-windows/repak/temp/repak_cli-x86_64-unknown-linux-gnu.tar.xz -fL "https://github.com/trumank/repak/releases/latest/download/repak_cli-x86_64-unknown-linux-gnu.tar.xz"
 
 
-pushd .\bin\Release\net8.0\repak\temp
+pushd .\bin\Release\net8.0-windows\repak\temp
 
 "C:\Program Files\7-Zip\7z.exe" x "repak_cli-x86_64-unknown-linux-gnu.tar.xz" 
 "C:\Program Files\7-Zip\7z.exe" x "repak_cli-x86_64-unknown-linux-gnu.tar" 
@@ -20,7 +20,7 @@ move repak ..\..\..\repak.elf
 popd
 
 
-pushd .\bin\Release\net8.0\repak\temp\windows
+pushd .\bin\Release\net8.0-windows\repak\temp\windows
 
 tar -xf repak_cli-x86_64-pc-windows-msvc.zip
 move repak.exe ..\..\..\
@@ -29,7 +29,7 @@ move repak.exe ..\..\..\
 popd 
 
 
-rd /s /q ".\bin\Release\net8.0\repak\"
+rd /s /q ".\bin\Release\net8.0-windows\repak\"
 
 call iscc /O./Output /Q InstallerScript.iss 
 pause
